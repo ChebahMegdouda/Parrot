be97960b1ea855a92ed79a28cb3ba7748278be06
Megdouda <Megdouda@164.81.232.166>
2018-11-20 09:35:51 +0100

    teste European Parrot

---
+++ kataparrot/src/main/java/iut/unilim/fr/kataparrot/EuropeanParrot.java
@@ -0,0 +1,9 @@
+package iut.unilim.fr.kataparrot;
+
+public class EuropeanParrot extends Parrot {
+	
+	public EuropeanParrot (ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
+			super (_type, numberOfCoconuts, voltage, isNailed);
+
+	}
+}
